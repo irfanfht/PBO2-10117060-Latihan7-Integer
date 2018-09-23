ac6a524a502d7cc074fc57002e427dc7b1e3c0e0
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 12:26:14 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117060/latihan7/integer/PBO210117060Latihan7Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117060.latihan7.integer;
+
+/**
+ *
+ * @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi menampilkan tentang jenis tipe data
+ * bilangan bulat.
+ */
+public class PBO210117060Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("Byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
